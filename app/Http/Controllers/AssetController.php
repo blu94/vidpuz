d90cbe9767d555102a7848be25f45462bbdc5598
YAP CHENG WEI <yapchengwei@gmail.com>
2017-07-05 11:17:28 +0800

    update

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
-class UserAssetController extends Controller
+class AssetController extends Controller
 {
     /**
      * Display a listing of the resource.
