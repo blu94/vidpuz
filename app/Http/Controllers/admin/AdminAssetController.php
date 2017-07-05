be0dfc83f697510f975bedac14e6a2fb53e12ce6
YAP CHENG WEI <yapchengwei@gmail.com>
2017-07-05 11:12:36 +0800

    update

--- app/Http/Controllers/admin/AdminAssetController.php
+++ app/Http/Controllers/admin/AdminAssetController.php
@@ -5,12 +5,6 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Asset;
-use Auth;
-
-use Carbon;
-use Thumbnail;
-
 class AdminAssetController extends Controller
 {
     /**
@@ -44,51 +38,6 @@ class AdminAssetController extends Controller
     public function store(Request $request)
     {
         //
-        $file = $request->file('file');
-
-        $name = time() . md5($file->getClientOriginalName()) . $file->getClientOriginalName();
-
-        $path = '/assets/' . $name;
-
-        $upload_status = $file->move('assets', $path);
-
-        $extension = pathinfo($path, PATHINFO_EXTENSION);
-
-
-
-        if($upload_status) {
-
-          $thumbnail_path   = '/assets';
-
-          $video_path       = $path;
-
-          // set thumbnail image name
-          $thumbnail_image  = time() . $name.".jpg";
-
-          // get video length and process it
-          // assign the value to time_to_image (which will get screenshot of video at that specified seconds)
-          $time_to_image    = floor(($data['video_length'])/2);
-
-
-          $thumbnail_status = Thumbnail::getThumbnail($video_path,$thumbnail_path,$thumbnail_image,$time_to_image);
-          if($thumbnail_status)
-          {
-            echo "Thumbnail generated";
-          }
-          else
-          {
-            echo "thumbnail generation has failed";
-          }
-        }
-
-        $asset = Asset::create([
-          'title' => $name,
-          'path' => $path,
-          'format' => $extension,
-          'user_id' => Auth::user()->id
-        ]);
-        //
-        // echo $asset->id;
     }
 
     /**
